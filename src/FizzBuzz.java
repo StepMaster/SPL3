c4f9e00fb240aa73df038ae2b011fe44862fe451
StepMaster <171wita26@o356.htl-leoben.at>
2018-05-07 14:44:50 +0200

    FizzBuzz, Version 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,23 +3,27 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 100; i++) {
-			FizzerBuzzer(i);
+			FizzerBuzzer(i, true);
+		}
+		System.out.println("-----------------------------");
+		for(int i = 4; i<=100; i+=4) {
+			FizzerBuzzer(i, false);
 		}
 		
 		
 		
 	}
 	
-	public static void FizzerBuzzer(int i) {
+	public static void FizzerBuzzer(int i, boolean showFizzBuzz) {
 
-		if (i % 4 == 0 && i % 7 == 0) {
-			System.out.println("FizzerBuzzer");
+		if (i % 4 == 0 && i % 7 == 0 && showFizzBuzz == true) {
+			System.out.println("FizzerBuzzer ("+i+")");
 
 		} else if (i % 4 == 0) {
-			System.out.println("Fizzer");
+			System.out.println("Fizzer ("+i+")");
 
 		} else if (i % 7 == 0) {
-			System.out.println("Buzzer");
+			System.out.println("Buzzer ("+i+")");
 
 		} else {
 			System.out.println(i);
